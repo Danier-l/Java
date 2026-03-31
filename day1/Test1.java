b91459143a4c270b36fdffd15aeb68981a2da9af
Danier-l <1685132202@qq.com>
2026-03-31 22:10:20 +0800

    Add files via upload

--- day1/Test1.java
+++ day1/Test1.java
@@ -1,5 +1,93 @@
+import java.util.Scanner;
+
+
+
 public class Test1 {
-    public static void main() {
-        System.out.println("刘家硕好香");
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+
+//        //bmi
+//        System.out.println("请输入身高");
+//        double height=sc.nextInt();
+//        System.out.println("请输入体重");
+//        double weight=sc.nextInt();
+//        double bmi=weight/(height*height);
+//        if(bmi>0){
+//            if(bmi<18.5){
+//                System.out.println("身体状况：消瘦，健康风险：部分增加");
+//            }
+//            if(bmi>=18.5&&bmi<23.9){
+//                System.out.println("身体状况：消瘦，健康风险：部分增加");
+//            }
+//            if(bmi>24.0&&bmi<26.9){
+//                System.out.println("身体状况：消瘦，健康风险：部分增加");
+//            }
+//            if(bmi>27.0&&bmi<29.9){
+//                System.out.println("身体状况：消瘦，健康风险：部分增加");
+//            }
+//            if(bmi>=30.0){
+//                System.out.println("身体状况：消瘦，健康风险：部分增加");
+//            }else{
+//                System.out.println("错误");
+//            }
+
+
+//        //if 判断  判断是什么三角形
+//        System.out.println("输入第一个边");
+//        double a = sc.nextDouble();
+//        System.out.println("输入第二个边");
+//        double b = sc.nextDouble();
+//        System.out.println("输入第三个边");
+//        double c = sc.nextDouble();
+//        if (a + b > c && a + c > b && b + c > a) {
+//            if (a == b && a == c) {
+//                System.out.println("三角行是等边三角形");
+//            } else if ((a == b && a != c) || (a == c && a != b) || (b == c && a != c)) {
+//                System.out.println("三角行是等腰三角形");
+//            } else if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b)) {
+//                System.out.println("三角行是直角三角形");
+//            }else {
+//                System.out.println("三角形是普通三角形");
+//            }
+//
+//        } else {
+//            System.out.println(" 出错了");
+//        }
+
+
+        //switch  选择 星期计划
+//        System.out.println("输入今天是星期几");
+//        int day=sc.nextInt();
+//        switch(day){
+//            case 1:
+//                System.out.println("今天去跑步");
+//                break;
+//            case 2:
+//                System.out.println("今天去游泳");
+//                break;
+//            case 3:
+//                System.out.println("今天去慢走");
+//                break;
+//            case 4:
+//                System.out.println("今天去单车");
+//                break;
+//            case 5:
+//                System.out.println("今天去拳击");
+//                break;
+//            case 6:
+//                System.out.println("今天去爬山");
+//                break;
+//            case 7:
+//                System.out.println("今天吃放纵餐");
+//                break;
+//            default:
+//                System.out.println("出错了");
+//                break;
+//        }
+
+        //switch 选择
+
+
     }
 }
